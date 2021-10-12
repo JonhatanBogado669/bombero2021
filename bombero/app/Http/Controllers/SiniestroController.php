f0ba99a17da6052c5a3ebbca6c294752aeb84804
Jonhatan Bogado <jonhatanbogado@gmail.com>
2021-10-12 11:58:15 -0300

    creacion de models y controllers

---
+++ bombero/app/Http/Controllers/SiniestroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siniestro;
+use Illuminate\Http\Request;
+
+class SiniestroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Siniestro  $siniestro
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siniestro $siniestro)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Siniestro  $siniestro
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siniestro $siniestro)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Siniestro  $siniestro
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Siniestro $siniestro)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Siniestro  $siniestro
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Siniestro $siniestro)
+    {
+        //
+    }
+}
